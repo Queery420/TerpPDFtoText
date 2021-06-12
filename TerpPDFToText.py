745f41732e635b6e93057298beb721c9cb3c7624
Queeryeverything <46384594+Queeryeverything@users.noreply.github.com>
2021-06-11 21:54:47 -0400

    v0.0.0 - Code Logic: yes; Code: no

--- TerpPDFToText.py
+++ TerpPDFToText.py
@@ -2,8 +2,17 @@
 # Written by Ivy Slick (@Queery420) on 6/11/21
 # This code uses TeT (https://www.pdflib.com/products/tet/overview/)
 #
-# This program extracts text from Terpene Data PDFs
-# from growers and manufactures operating within
-# the Maryland Medical Cannabis Industry and
-# formats it for use in labeling or posting to
+# This program extracts text from Cannabanoid and Terpene Content PDFs
+# from growers and manufactures operating within the Maryland Medical
+# Cannabis Industry and formats it for use in labeling or posting to
 # a website.
+#
+# Code logic
+# >Scan program folder for PDFs
+# >Extract Text & Tables from PDFs
+# >Search extracted data for:
+# >>Strain Name
+# >>RFID
+# >>Cannabanoid Content
+# >>Terpene Content
+# >Format and Print Found information to text files
